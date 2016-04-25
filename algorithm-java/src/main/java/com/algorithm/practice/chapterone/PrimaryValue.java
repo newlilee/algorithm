ef1fc999cf7dc8a91d695694cd0cb84eae239263
clx <brianchen.clx@gmail.com>
2016-04-25 13:30:05 +0800

    commit practice

---
+++ algorithm-java/src/main/java/com/algorithm/practice/chapterone/PrimaryValue.java
@@ -0,0 +1,11 @@
+package com.algorithm.practice.chapterone;
+
+/**
+ * @author chenlixin at 2016年4月25日 下午1:29:31
+ */
+public class PrimaryValue {
+
+    public static void main(String[] args) {
+        
+    }
+}
