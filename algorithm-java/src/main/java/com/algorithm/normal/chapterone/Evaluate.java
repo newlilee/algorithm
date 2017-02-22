4c0868b66e2d6b64382c88fb3b0931ade3376c19
brianchen.clx <brianchen.clx@gmail.com>
2017-02-22 17:19:08 +0800

    use stack calculate expression's value

---
+++ algorithm-java/src/main/java/com/algorithm/normal/chapterone/Evaluate.java
@@ -0,0 +1,52 @@
+package com.algorithm.normal.chapterone;
+
+import edu.princeton.cs.algs4.StdIn;
+
+/**
+ * @author chenlixin at 2017年2月22日 上午11:57:20
+ */
+public class Evaluate {
+
+    /**
+     * two stack calculate expression's value
+     */
+    public static void calculate() {
+        Stack<String> ops = new Stack<String>();
+        Stack<Double> vals = new Stack<Double>();
+        while (!StdIn.isEmpty()) {
+            String str = StdIn.readString();
+            if (str.equals("(")) {
+            } else if (str.equals("+")) {
+                ops.push(str);
+            } else if (str.equals("-")) {
+                ops.push(str);
+            } else if (str.equals("*")) {
+                ops.push(str);
+            } else if (str.equals("/")) {
+                ops.push(str);
+            } else if (str.equals("sqrt")) {
+                ops.push(str);
+            } else if (str.equals(")")) {
+                String op = (String) ops.pop();
+                Double v = (Double) vals.pop();
+                if (op.equals("+")) {
+                    v = (Double) vals.pop() + v;
+                } else if (op.equals("-")) {
+                    v = (Double) vals.pop() - v;
+                } else if (op.equals("*")) {
+                    v = (Double) vals.pop() * v;
+                } else if (op.equals("/")) {
+                    v = (Double) vals.pop() / v;
+                } else if (op.equals("sqrt")) {
+                    v = Math.sqrt(v);
+                }
+            } else {
+                vals.push(Double.parseDouble(str));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
