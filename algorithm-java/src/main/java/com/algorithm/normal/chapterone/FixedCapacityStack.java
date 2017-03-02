1236b6d716087721fbbec2e47da71780ba2421c1
brianchen.clx <brianchen.clx@gmail.com>
2017-03-02 10:23:11 +0800

    fixed capacity stack with gerneric

---
+++ algorithm-java/src/main/java/com/algorithm/normal/chapterone/FixedCapacityStack.java
@@ -0,0 +1,30 @@
+package com.algorithm.normal.chapterone;
+
+/**
+ * @author chenlixin at 2017年3月2日 上午10:05:21
+ */
+public class FixedCapacityStack<T> {
+
+    private T[] array;
+    private int len;
+
+    public FixedCapacityStack(Integer cap) {
+        array = (T[]) new Object[cap];
+    }
+
+    public void push(T t) {
+        array[len++] = t;
+    }
+
+    public T pop() {
+        return array[--len];
+    }
+
+    public boolean isEmpty() {
+        return len == 0;
+    }
+
+    public int size() {
+        return len;
+    }
+}
