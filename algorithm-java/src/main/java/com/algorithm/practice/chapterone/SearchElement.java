1aed2abb384613fc0d5134bd8b47ceef3dc80b02
clx <brianchen.clx@gmail.com>
2018-03-07 10:16:39 +0800

    add first great than element

--- algorithm-java/src/main/java/com/algorithm/practice/chapterone/SearchElement.java
+++ algorithm-java/src/main/java/com/algorithm/practice/chapterone/SearchElement.java
@@ -8,17 +8,14 @@ package com.algorithm.practice.chapterone;
 public class SearchElement {
 
 	/**
-	 * find first element great than target
+	 * binary search
 	 *
 	 * @param array  sorted array
 	 * @param target target number
-	 * @return first element great than target
+	 * @return result number
 	 */
-	private static int firstGreatThanElement(int[] array, int target) {
-		if (array == null) {
-			return -1;
-		}
-		if (array.length == 0) {
+	private static int binarySearch(int[] array, int target) {
+		if (array == null || array.length == 0) {
 			return -1;
 		}
 		int low = 0;
@@ -36,9 +33,45 @@ public class SearchElement {
 		return -1;
 	}
 
+	/**
+	 * find first element great than target
+	 *
+	 * @param array  sorted array,sort type desc
+	 * @param target target number
+	 * @return first element great than target
+	 */
+	private static int firstGreatThanElement(final int[] array, int target) {
+		if (array == null || array.length == 0) {
+			return -1;
+		}
+
+		int low = 0;
+		int high = array.length - 1;
+		while (low < high) {
+			int mid = (low + high) >>> 1;
+			if (array[mid] >= target) {
+				high = mid;
+			} else {
+				low = mid + 1;
+			}
+		}
+
+		if (array[low] <= array[high]) {
+			if (target <= array[low]) {
+				return array[low];
+			} else if (array[low] < target && target <= array[high]) {
+				return array[high];
+			} else {
+				return -1;
+			}
+		} else {
+			return -1;
+		}
+	}
+
 	public static void main(String[] args) {
-		int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-		int key = 6;
+		int[] array = new int[]{1, 2, 3, 4, 5, 7, 8, 10};
+		int key = 0;
 		System.out.println(firstGreatThanElement(array, key));
 	}
 }
