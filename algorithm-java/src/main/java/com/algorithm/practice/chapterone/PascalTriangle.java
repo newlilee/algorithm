f4d589cb371541e6670c75fecf84c79082ccc014
clx <brianchen.clx@gmail.com>
2018-03-23 18:09:52 +0800

    add practice

---
+++ algorithm-java/src/main/java/com/algorithm/practice/chapterone/PascalTriangle.java
@@ -0,0 +1,23 @@
+package com.algorithm.practice.chapterone;
+
+/**
+ * Pascal Triangle
+ * 
+ * @author clx 2018/3/21.
+ */
+public class PascalTriangle {
+
+	public static void main(String[] args) {
+		int depth = 9;
+		pascalTriangle(depth);
+	}
+
+	/**
+	 * print pacal triangle
+	 * 
+	 * @param depth
+	 */
+	private static void pascalTriangle(int depth) {
+
+	}
+}
