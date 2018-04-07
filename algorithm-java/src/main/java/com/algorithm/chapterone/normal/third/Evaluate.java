adfd2bc0fd2e1b1a600acdf74a610849b5fe4a92
clx <brianchen.clx@gmail.com>
2018-04-07 15:50:57 +0800

    fix bug

--- algorithm-java/src/main/java/com/algorithm/chapterone/normal/third/Evaluate.java
+++ algorithm-java/src/main/java/com/algorithm/chapterone/normal/third/Evaluate.java
@@ -1,19 +1,24 @@
 package com.algorithm.chapterone.normal.third;
 
-import com.algorithm.chapterone.normal.third.Stack;
 import edu.princeton.cs.algs4.StdIn;
 
 /**
+ * calculate expression's value
+ * 
  * @author chenlixin at 2017年2月22日 上午11:57:20
  */
 public class Evaluate {
 
+	public static void main(String[] args) {
+		Evaluate.calculate();
+	}
+
 	/**
 	 * two stack calculate expression's value
 	 */
-	public static void calculate() {
-		Stack<String> ops = new Stack<String>();
-		Stack<Double> vals = new Stack<Double>();
+	private static void calculate() {
+		Stack<String> ops = new Stack<>();
+		Stack<Double> values = new Stack<>();
 		while (!StdIn.isEmpty()) {
 			String str = StdIn.readString();
 			if (str.equals("(")) {
@@ -29,25 +34,22 @@ public class Evaluate {
 				ops.push(str);
 			} else if (str.equals(")")) {
 				String op = ops.pop();
-				Double v = vals.pop();
+				Double v = values.pop();
 				if (op.equals("+")) {
-					v = vals.pop() + v;
+					v = values.pop() + v;
 				} else if (op.equals("-")) {
-					v = vals.pop() - v;
+					v = values.pop() - v;
 				} else if (op.equals("*")) {
-					v = vals.pop() * v;
+					v = values.pop() * v;
 				} else if (op.equals("/")) {
-					v = vals.pop() / v;
+					v = values.pop() / v;
 				} else if (op.equals("sqrt")) {
 					v = Math.sqrt(v);
 				}
+				values.push(v);
 			} else {
-				vals.push(Double.parseDouble(str));
+				values.push(Double.parseDouble(str));
 			}
 		}
-	}
-
-	public static void main(String[] args) {
-
 	}
 }
