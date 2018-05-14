bd7f24d587f904d8271bc02053c2637f345a0678
clx <brianchen.clx@gmail.com>
2018-05-14 16:43:50 +0800

    change testcase

--- src/test/java/com/algorithm/chapterone/normal/fourth/TestThreeSum.java
+++ src/test/java/com/algorithm/chapterone/normal/fourth/TestThreeSum.java
@@ -1,6 +1,5 @@
 package com.algorithm.chapterone.normal.fourth;
 
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 /**
@@ -8,7 +7,7 @@ import edu.princeton.cs.algs4.StdOut;
  */
 public class TestThreeSum {
 	public static void main(String[] args) {
-		int[] array = StdIn.readAllInts();
+		int[] array = { 1, 2, 4, 5, 6, -8, -9, -11, 3, 2, 5, 7, 8, -2 };
 		StdOut.println(ThreeSum.count(array));
 	}
 }
