b80e72e68c60643c50904ed38ce63d723898b07f
clx <brianchen.clx@gmail.com>
2018-05-15 10:20:14 +0800

    add mills/nano time

--- src/main/java/com/algorithm/chapterone/normal/fourth/Stopwatch.java
+++ src/main/java/com/algorithm/chapterone/normal/fourth/Stopwatch.java
@@ -10,18 +10,43 @@ public class Stopwatch {
 	 * startTime
 	 */
 	private final long startTime;
+	/**
+	 * startNanoTime
+	 */
+	private final long startNanoTime;
 
 	public Stopwatch() {
 		this.startTime = System.currentTimeMillis();
+		this.startNanoTime = System.nanoTime();
 	}
 
 	/**
-	 * elapsed time
+	 * elapsed seconds time
 	 * 
 	 * @return
 	 */
-	public double elapsedTime() {
+	public double elapsedSecondTime() {
 		long now = System.currentTimeMillis();
 		return (now - startTime) / 1000.0;
+	}
+
+	/**
+	 * elapsed millis time
+	 * 
+	 * @return
+	 */
+	public double elapsedMillisTime() {
+		long now = System.currentTimeMillis();
+		return now - startTime;
+	}
+
+	/**
+	 * elapsed nanoTime
+	 * 
+	 * @return
+	 */
+	public long elapsedNanoTime() {
+		long now = System.nanoTime();
+		return now - startNanoTime;
 	}
 }
