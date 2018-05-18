17eb8e46d9e536eef87089bf6a89f5c2a92e1c31
clx <brianchen.clx@gmail.com>
2018-05-18 18:25:35 +0800

    modified union-find testcase

--- src/test/java/com/algorithm/chapterone/normal/fifth/TestUnionFind.java
+++ src/test/java/com/algorithm/chapterone/normal/fifth/TestUnionFind.java
@@ -1,5 +1,7 @@
 package com.algorithm.chapterone.normal.fifth;
 
+import edu.princeton.cs.algs4.StdIn;
+
 /**
  * union-find test
  * 
@@ -7,4 +9,18 @@ package com.algorithm.chapterone.normal.fifth;
  */
 public class TestUnionFind {
 
+	public static void main(String[] args) {
+		int count = StdIn.readInt();
+		UnionFind unionFind = new UnionFind(count);
+		while (!StdIn.isEmpty()) {
+			int p = StdIn.readInt();
+			int q = StdIn.readInt();
+			if (unionFind.connected(p, q)) {
+				continue;
+			}
+			unionFind.union(p, q);
+			System.out.println("p:" + p + ",q:" + q);
+		}
+		System.out.println(unionFind.count() + "components");
+	}
 }
