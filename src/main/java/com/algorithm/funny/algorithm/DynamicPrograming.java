1e613dc9d64d8c83ee82a6c650de81dea909e9e3
clx <brianchen.clx@gmail.com>
2018-05-23 10:31:59 +0800

    add note algorithm

--- src/main/java/com/algorithm/funny/algorithm/DynamicPrograming.java
+++ src/main/java/com/algorithm/funny/algorithm/DynamicPrograming.java
@@ -1,5 +1,7 @@
 package com.algorithm.funny.algorithm;
 
+import java.util.Map;
+
 /**
  * 动态规划：<br/>
  * 要点： 1.最优子结构 2.边界 3.状态转移公式
@@ -14,7 +16,7 @@ public class DynamicPrograming {
 	 * @param n
 	 * @return
 	 */
-	public static int steps(int n) {
+	public static int getClimbingWays(int n) {
 		if (n < 1) {
 			return 0;
 		}
@@ -24,6 +26,32 @@ public class DynamicPrograming {
 		if (n == 2) {
 			return 2;
 		}
-		return steps(n - 1) + steps(n - 2);
+		return getClimbingWays(n - 1) + getClimbingWays(n - 2);
+	}
+
+	/**
+	 * 备忘录算法
+	 * 
+	 * @param n
+	 * @param stepMap
+	 * @return
+	 */
+	public static int getClimbingWays(int n, Map<Integer, Integer> stepMap) {
+		if (n < 1) {
+			return 0;
+		}
+		if (n == 1) {
+			return 1;
+		}
+		if (n == 2) {
+			return 2;
+		}
+		if (stepMap.containsKey(n)) {
+			return stepMap.get(n);
+		} else {
+			int value = getClimbingWays(n - 1) + getClimbingWays(n - 2);
+			stepMap.put(n, value);
+			return value;
+		}
 	}
 }
--- src/test/java/com/algorithm/funny/algorithm/TestDynamicPrograming.java
+++ src/test/java/com/algorithm/funny/algorithm/TestDynamicPrograming.java
@@ -1,12 +1,21 @@
 package com.algorithm.funny.algorithm;
 
+import com.algorithm.chapterone.normal.fourth.Stopwatch;
+
+import java.util.HashMap;
+
 /**
  * @author clx 2018/5/23.
  */
 public class TestDynamicPrograming {
 
 	public static void main(String[] args) {
-		int steps = 10;
-		System.out.println(DynamicPrograming.steps(steps));
+		int steps = 20;
+		Stopwatch stopwatch = new Stopwatch();
+		System.out.println(DynamicPrograming.getClimbingWays(steps));
+		System.out.println(stopwatch.elapsedNanoTime());
+		stopwatch = new Stopwatch();
+		System.out.println(DynamicPrograming.getClimbingWays(steps, new HashMap<Integer, Integer>()));
+		System.out.println(stopwatch.elapsedNanoTime());
 	}
 }
