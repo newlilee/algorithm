908a760fc294a5d700e80aa555b8f8e09be53385
clx <brianchen.clx@gmail.com>
2018-06-05 18:23:42 +0800

    pathcompression uf

---
+++ src/main/java/com/algorithm/chapterone/normal/fifth/ver/fourth/PathCompressionUninoFind.java
@@ -0,0 +1,25 @@
+package com.algorithm.chapterone.normal.fifth.ver.fourth;
+
+/**
+ * 路径压缩 union find
+ * 
+ * @author clx 2018/6/5.
+ */
+public class PathCompressionUninoFind {
+
+	/**
+	 * 分量id
+	 */
+	private int[] id;
+	/**
+	 * 各根节点所对应的分量大小
+	 */
+	private int[] sz;
+	/**
+	 * 分量count
+	 */
+	private int count;
+
+
+	
+}
