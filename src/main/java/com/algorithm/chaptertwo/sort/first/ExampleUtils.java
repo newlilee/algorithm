78b1b5f55b5fbeae45664a74924d107b0895fd48
clx <brianchen.clx@gmail.com>
2018-06-28 11:31:25 +0800

    modified ExampleUtils

--- src/main/java/com/algorithm/chaptertwo/sort/first/ExampleUtils.java
+++ src/main/java/com/algorithm/chaptertwo/sort/first/ExampleUtils.java
@@ -7,67 +7,67 @@ import edu.princeton.cs.algs4.StdOut;
  */
 public class ExampleUtils {
 
-    /**
-     * sort method
-     * 
-     * @param array
-     */
-    public static void sort(Comparable[] array) {
+	/**
+	 * sort method
+	 * 
+	 * @param array
+	 */
+	public static void sort(Comparable[] array) {
 
-    }
+	}
 
-    /**
-     * compare sorted
-     * 
-     * @param v
-     * @param w
-     * @return
-     */
-    public static boolean less(Comparable v, Comparable w) {
-        return v.compareTo(w) < 0;
-    }
+	/**
+	 * compare sorted
+	 * 
+	 * @param v
+	 * @param w
+	 * @return
+	 */
+	public static boolean less(Comparable v, Comparable w) {
+		return v.compareTo(w) < 0;
+	}
 
-    /**
-     * exchange element
-     * 
-     * @param array
-     * @param i
-     * @param j
-     */
-    public static void exch(Comparable[] array, int i, int j) {
-        Comparable temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
+	/**
+	 * exchange element
+	 * 
+	 * @param array
+	 * @param i
+	 * @param j
+	 */
+	public static void exch(Comparable[] array, int i, int j) {
+		Comparable temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
 
-    /**
-     * print array
-     * 
-     * @param array
-     */
-    public static void printArray(Comparable[] array) {
-        for (int i = 0; i < array.length; i++) {
-            StdOut.print(array[i] + " ");
-        }
-        StdOut.println();
-    }
+	/**
+	 * print array
+	 * 
+	 * @param array
+	 */
+	public static void printArray(Comparable[] array) {
+		for (int i = 0; i < array.length; i++) {
+			StdOut.print(array[i] + " ");
+		}
+		StdOut.println();
+	}
 
-    /**
-     * judge array is or not sorted
-     * 
-     * @param array
-     * @return
-     */
-    public static boolean isSorted(Comparable[] array) {
-        for (int i = 0; i < array.length; i++) {
-            if (ExampleUtils.less(array[i], array[i - 1])) {
-                return false;
-            }
-        }
-        return true;
-    }
+	/**
+	 * judge array is or not sorted
+	 * 
+	 * @param array
+	 * @return
+	 */
+	public static boolean isSorted(Comparable[] array) {
+		for (int i = 0; i < array.length; i++) {
+			if (ExampleUtils.less(array[i], array[i - 1])) {
+				return false;
+			}
+		}
+		return true;
+	}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-    }
+	}
 }
