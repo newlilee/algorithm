af3fdd36615614588f707adba76722b758d98431
clx <brianchen.clx@gmail.com>
2019-02-20 12:53:17 +0800

    add 3way quick sort

---
+++ src/main/java/com/algorithm/chaptertwo/sort/third/QuickSortThreeWay.java
@@ -0,0 +1,51 @@
+package com.algorithm.chaptertwo.sort.third;
+
+import com.algorithm.chaptertwo.sort.util.SortUtils;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+/**
+ * @author clx 2/20/2018
+ */
+public class QuickSortThreeWay {
+
+	/**
+	 * quick sort
+	 * 
+	 * @param array
+	 */
+	public static void sort(Comparable[] array) {
+		if (SortUtils.checkArrayValid(array)) {
+			return;
+		}
+		StdRandom.shuffle(array);
+		sort(array, 0, array.length - 1);
+	}
+
+	/**
+	 * sort
+	 * 
+	 * @param array
+	 * @param lo
+	 * @param hi
+	 */
+	private static void sort(Comparable[] array, int lo, int hi) {
+		if (hi <= lo) {
+			return;
+		}
+		int lt = lo, idx = lo + 1, gt = hi;
+		Comparable pivot = array[lo];
+		while (idx <= gt) {
+			int cmp = array[idx].compareTo(pivot);
+			if (cmp < 0) {
+				SortUtils.exch(array, lt++, idx++);
+			} else if (cmp > 0) {
+				SortUtils.exch(array, idx, gt--);
+			} else {
+				idx++;
+			}
+		}
+		sort(array, lo, lt - 1);
+		sort(array, gt + 1, hi);
+	}
+}
