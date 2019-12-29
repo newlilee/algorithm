ebf0b75f2e7fbfb861f06bfa8be9026145ed1da5
clx <brianchen.clx@gmail.com>
2019-12-29 13:14:01 +0800

    add:median of two sorted array

--- src/main/java/com/leetcode/MedianTwoSortedArray.java
+++ src/main/java/com/leetcode/MedianTwoSortedArray.java
@@ -20,36 +20,46 @@ public class MedianTwoSortedArray {
 			return -1;
 		}
 		int mergedLen = 0;
-		if (firstArr == null){
-
+		if (firstArr == null) {
+			mergedLen = secondArr.length;
+		} else if (secondArr == null) {
+			mergedLen = firstArr.length;
+		} else {
+			mergedLen = firstArr.length + secondArr.length;
 		}
-		int[] mergedArr = new int[firstArr.length + secondArr.length];
+		int[] mergedArr = new int[mergedLen];
 		int idx = 0;
 		int jdx = 0;
 		int kdx = 0;
 		while (idx < firstArr.length || jdx < secondArr.length) {
-			if (secondArr.length == 0) {
-				mergedArr = firstArr;
-				break;
+			if (idx == firstArr.length) {
+				mergedArr[kdx++] = secondArr[jdx];
+				jdx++;
+				continue;
 			}
-			if (firstArr.length == 0) {
-				mergedArr = secondArr;
-				break;
+			if (jdx == secondArr.length) {
+				mergedArr[kdx++] = firstArr[idx];
+				idx++;
+				continue;
 			}
 
 			if (firstArr[idx] < secondArr[jdx]) {
-				mergedArr[kdx++] = firstArr[idx++];
+				mergedArr[kdx++] = firstArr[idx];
+				idx++;
 			} else if (firstArr[idx] > secondArr[jdx]) {
-				mergedArr[kdx++] = secondArr[jdx++];
+				mergedArr[kdx++] = secondArr[jdx];
+				jdx++;
 			} else {
-				mergedArr[kdx++] = firstArr[idx++];
-				mergedArr[kdx++] = secondArr[jdx++];
+				mergedArr[kdx++] = firstArr[idx];
+				mergedArr[kdx++] = secondArr[jdx];
+				idx++;
+				jdx++;
 			}
 		}
 
 		int len = mergedArr.length;
 		if (len % 2 == 0) {
-			return (mergedArr[len / 2 - 1] + mergedArr[len / 2]) / 2;
+			return ((double) mergedArr[len / 2 - 1] + mergedArr[len / 2]) / 2;
 		} else {
 			return mergedArr[len / 2];
 		}
