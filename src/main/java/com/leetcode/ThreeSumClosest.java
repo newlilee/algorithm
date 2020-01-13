514956cdcf3e1019800c45f1463d95b27a314748
clx <brianchen.clx@gmail.com>
2020-01-13 21:38:35 +0800

    fix

---
+++ src/main/java/com/leetcode/ThreeSumClosest.java
@@ -0,0 +1,19 @@
+package com.leetcode;
+
+/**
+ * 16. 3Sum Closest
+ *
+ * @author clx
+ */
+public class ThreeSumClosest {
+
+	/**
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public static int threeSumClosest(int[] nums, int target) {
+
+		return -1;
+	}
+}
---
+++ src/test/java/com/leetcode/TestThreeSum.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author clx
+ */
+public class TestThreeSum {
+
+	@Test
+	public void testThreeSum() {
+		int[] array = new int[]{-1, 0, 1, 2, -1, -4};
+		List<List<Integer>> result = ThreeSum.threeSum(array);
+		System.out.println(result);
+	}
+}
