c45d5987716f7c23cea500a43172785da0d67eeb
clx <brianchen.clx@gmail.com>
2020-01-16 23:59:44 +0800

    fix

---
+++ src/test/java/com/leetcode/TestValidParentheses.java
@@ -0,0 +1,16 @@
+package com.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author clx
+ */
+public class TestValidParentheses {
+
+	@Test
+	public void testValid() {
+		String content = "()[]{}";
+		boolean result = ValidParentheses.isValid(content);
+		System.out.println(result);
+	}
+}
