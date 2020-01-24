16b8bea43d3a7013094e391b7c44e456d63e1efd
clx <brianchen.clx@gmail.com>
2020-01-24 23:58:03 +0800

    add:swap node pair

---
+++ src/main/java/com/leetcode/SwapNodePair.java
@@ -0,0 +1,29 @@
+package com.leetcode;
+
+import com.leetcode.common.ListNode;
+
+/**
+ * 24. Swap Nodes in Pairs
+ *
+ * @author clx
+ */
+public class SwapNodePair {
+
+	/**
+	 * swap node pair brute force
+	 *
+	 * @param head
+	 * @return
+	 */
+	public static ListNode swapBruteForce(ListNode head) {
+		if (head == null) {
+			return null;
+		}
+		if (head.getNext() == null) {
+			return head;
+		}
+
+
+		return null;
+	}
+}
