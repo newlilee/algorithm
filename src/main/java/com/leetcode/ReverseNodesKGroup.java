09cc3d2522f3f61b4bf2e628aba6cfce4db4b4f3
clx <brianchen.clx@gmail.com>
2020-01-26 23:44:57 +0800

    add:recursive approach

--- src/main/java/com/leetcode/ReverseNodesKGroup.java
+++ src/main/java/com/leetcode/ReverseNodesKGroup.java
@@ -10,16 +10,49 @@ import com.leetcode.common.ListNode;
 public class ReverseNodesKGroup {
 
 	/**
-	 * reverse k group brute force
+	 * reverse k group use recursive
 	 *
 	 * @param head
 	 * @param k
 	 * @return
 	 */
-	public static ListNode reverseBruteForce(ListNode head, int k) {
+	public static ListNode reverseByRecursive(ListNode head, int k) {
 		if (head == null || head.getNext() == null || k <= 0) {
 			return head;
 		}
+		ListNode currNode = head;
+		int count = 0;
+		while (currNode != null && count != k) {
+			currNode = currNode.getNext();
+			count++;
+		}
+		if (count == k) {
+			currNode = reverseByRecursive(currNode, k);
+			while (count > 0) {
+				ListNode tmp = head.getNext();
+				head.setNext(currNode);
+				currNode = head;
+				head = tmp;
+				count--;
+			}
+			head = currNode;
+		}
+		return head;
+	}
+
+	/**
+	 * reverse k group use loop
+	 *
+	 * @param head
+	 * @param k
+	 * @return
+	 */
+	public static ListNode reverseByLoop(ListNode head, int k) {
+		if (head == null || head.getNext() == null || k <= 0) {
+			return head;
+		}
+
+
 
 		return null;
 	}
--- src/test/java/com/leetcode/TestReverseNodesKGroup.java
+++ src/test/java/com/leetcode/TestReverseNodesKGroup.java
@@ -9,12 +9,22 @@ import org.junit.Test;
 public class TestReverseNodesKGroup {
 
 	@Test
-	public void testReverseBruteForce() {
+	public void testReverseRecursive() {
+		ListNode head = new ListNode(1);
+		head.setNext(2).setNext(3).setNext(4).setNext(5).setNext(6).setNext(7);
+		System.out.println("original:" + head);
+		int k = 3;
+		ListNode result = ReverseNodesKGroup.reverseByRecursive(head, k);
+		System.out.println("swapped:" + result);
+	}
+
+	@Test
+	public void testReverseLoop() {
 		ListNode head = new ListNode(1);
 		head.setNext(2).setNext(3).setNext(4).setNext(5);
 		System.out.println("original:" + head);
 		int k = 3;
-		ListNode result = ReverseNodesKGroup.reverseBruteForce(head, k);
-		System.out.println("swapped:" + result);
+		ListNode result = ReverseNodesKGroup.reverseByLoop(head, k);
+		System.out.println("swapped:" + head);
 	}
 }
