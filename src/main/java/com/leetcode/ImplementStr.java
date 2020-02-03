f5bb19eb5f0a966377509e42d9ff6886e06e689f
clx <brianchen.clx@gmail.com>
2020-02-04 01:46:46 +0800

    add:impl str func

---
+++ src/main/java/com/leetcode/ImplementStr.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 28. Implement strStr()
+ *
+ * @author clx
+ */
+public class ImplementStr {
+
+	/**
+	 * @param haystack
+	 * @param needle
+	 * @return
+	 */
+	public static int strStr(String haystack, String needle) {
+		if (StringUtils.isBlank(haystack)) {
+			return -1;
+		}
+		if (StringUtils.isBlank(needle)) {
+			return 0;
+		}
+		int len = needle.length();
+		for (int idx = 0; idx < haystack.length(); idx++) {
+			int endIdx = idx + len;
+			if (endIdx > haystack.length()) {
+				return -1;
+			}
+			if (needle.equals(haystack.substring(idx, endIdx))) {
+				return idx;
+			}
+		}
+		return -1;
+	}
+}
