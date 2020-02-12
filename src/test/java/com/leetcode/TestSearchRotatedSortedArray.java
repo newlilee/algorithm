d2c1dfaf2acc35e9ffc7c565943622ba0846eeff
clx <brianchen.clx@gmail.com>
2020-02-12 11:11:53 +0800

    add:search rotated sorted array

---
+++ src/test/java/com/leetcode/TestSearchRotatedSortedArray.java
@@ -0,0 +1,17 @@
+package com.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author clx
+ */
+public class TestSearchRotatedSortedArray {
+
+	@Test
+	public void testSearchArray() {
+		int[] array = new int[]{4, 5, 6, 7, 0, 1, 2};
+		int target = 0;
+		int result = SearchRotatedSortedArray.search(array, target);
+		System.out.println("result:" + result);
+	}
+}
