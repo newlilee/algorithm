7aa4ff480128c8a0bf0951eba23c262b3702d5ed
clx <brianchen.clx@gmail.com>
2020-02-15 22:16:41 +0800

    add:valid sudoku

---
+++ src/test/java/com/leetcode/TestValidSudoku.java
@@ -0,0 +1,26 @@
+package com.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author clx
+ */
+public class TestValidSudoku {
+
+	@Test
+	public void testValidSudoku() {
+		char[][] board = new char[][]{
+				{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+				{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+				{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+				{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+				{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+				{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+				{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+				{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+				{'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+		};
+		boolean result = ValidSudoku.validSudoku(board);
+		System.out.println("result:" + result);
+	}
+}
