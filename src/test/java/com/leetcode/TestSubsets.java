90a8bc80027e5b42ba004d82820230304997d447
clx <brianchen.clx@gmail.com>
2020-02-22 23:57:21 +0800

    add:subsets

---
+++ src/test/java/com/leetcode/TestSubsets.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author clx
+ */
+public class TestSubsets {
+
+	@Test
+	public void testSubsets() {
+		int[] array = new int[]{1, 2, 3};
+		List<List<Integer>> result = Subsets.subset(array);
+		System.out.println(result);
+	}
+}
