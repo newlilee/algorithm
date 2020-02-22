fc5105c0ba30054ef6bd7975466eb58b3cb631c9
clx <brianchen.clx@gmail.com>
2020-02-22 23:55:20 +0800

    fix bug

--- src/main/java/com/leetcode/CombinationSum.java
+++ src/main/java/com/leetcode/CombinationSum.java
@@ -6,6 +6,14 @@ import java.util.List;
 
 /**
  * 39. Combination Sum
+ * Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+ * <p>
+ * The same repeated number may be chosen from candidates unlimited number of times.
+ * <p>
+ * Note:
+ * <p>
+ * All numbers (including target) will be positive integers.
+ * The solution set must not contain duplicate combinations.
  *
  * @author clx
  */
