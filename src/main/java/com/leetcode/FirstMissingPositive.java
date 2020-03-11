9214242080e596962d4e29ae66ae9704524a7d58
clx <brianchen.clx@gmail.com>
2020-03-11 13:41:44 +0800

    mod:optimize code

--- src/main/java/com/leetcode/FirstMissingPositive.java
+++ src/main/java/com/leetcode/FirstMissingPositive.java
@@ -20,9 +20,7 @@ public class FirstMissingPositive {
 		}
 		int idx = 0;
 		while (idx < array.length) {
-			if (array[idx] == idx + 1 || array[idx] <= 0 || array[idx] > array.length) {
-				idx++;
-			} else if (array[array[idx] - 1] != array[idx]) {
+			if (array[idx] > 0 && array[idx] <= array.length && array[array[idx] - 1] != array[idx]) {
 				ArrayUtils.exchange(array, idx, array[idx] - 1);
 			} else {
 				idx++;
--- src/test/java/com/leetcode/TestFirstMissingPositive.java
+++ src/test/java/com/leetcode/TestFirstMissingPositive.java
@@ -9,8 +9,8 @@ public class TestFirstMissingPositive {
 
 	@Test
 	public void testFistMissingPositive() {
-//		int[] array = new int[]{1, 2, 0};
-		int[] array = new int[]{3, 4, -1, 1};
+		int[] array = new int[]{1, 2, 0};
+//		int[] array = new int[]{3, 4, -1, 1};
 		int result = FirstMissingPositive.firstMissingPositive(array);
 		System.out.println("result:" + result);
 	}
