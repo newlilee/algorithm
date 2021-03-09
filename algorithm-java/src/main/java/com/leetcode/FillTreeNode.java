16c5e29aba7d6572c0342aeb83e8128177bf0010
clx <brianchen.clx@gmail.com>
2021-03-09 23:01:27 +0800

    add:add fill next tree node

---
+++ algorithm-java/src/main/java/com/leetcode/FillTreeNode.java
@@ -0,0 +1,56 @@
+package com.leetcode;
+
+import java.util.Objects;
+
+/**
+ * 116.填充每个节点的下一个右侧节点指针
+ *
+ * @author clx
+ */
+public class FillTreeNode {
+
+	/**
+	 * fill every node's next right node pointer
+	 *
+	 * @param root
+	 */
+	public static Node fillNextNode(Node root) {
+		if (Objects.isNull(root)) {
+			return null;
+		}
+		connectTwoNode(root.leftNode, root.rightNode);
+		return root;
+	}
+
+	private static void connectTwoNode(Node firstNode, Node secondNode) {
+		if (Objects.isNull(firstNode) || Objects.isNull(secondNode)) {
+			return;
+		}
+		// point to next node
+		firstNode.nextNode = secondNode;
+		// point to next node if have same parent node
+		connectTwoNode(firstNode.leftNode, firstNode.rightNode);
+		connectTwoNode(secondNode.leftNode, secondNode.rightNode);
+		// point to next node if haven't same parent node
+		connectTwoNode(firstNode.rightNode, secondNode.leftNode);
+	}
+
+	static class Node {
+		/**
+		 * value
+		 */
+		Integer val;
+		/**
+		 * left node
+		 */
+		Node leftNode;
+		/**
+		 * right node
+		 */
+		Node rightNode;
+		/**
+		 * next node
+		 */
+		Node nextNode;
+	}
+}
---
+++ algorithm-java/src/test/java/com/leetcode/TestFillTreeNode.java
@@ -0,0 +1,14 @@
+package com.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author clx
+ */
+public class TestFillTreeNode {
+
+	@Test
+	public void testFillNode() {
+		FillTreeNode.fillNextNode(null);
+	}
+}
