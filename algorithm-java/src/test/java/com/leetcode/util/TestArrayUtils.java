daa516c0cc835070f27db2bc9d3888d164e64753
clx <brianchen.clx@gmail.com>
2021-12-08 23:53:14 +0800

    add array init

---
+++ algorithm-java/src/test/java/com/leetcode/util/TestArrayUtils.java
@@ -0,0 +1,19 @@
+package com.leetcode.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author clx
+ */
+public class TestArrayUtils {
+
+	@Test
+	public void testInit() {
+		int[] arr = new int[5];
+		int initVal = 3;
+		ArrayUtils.initArray(arr, initVal);
+		Arrays.stream(arr).forEach(System.out::println);
+	}
+}
