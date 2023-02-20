004b8467e403bd57c20e35e1172e986dc86c23c7
clx <chenlixin@stars-lighting.com>
2023-02-20 18:10:15 +0800

    fix

--- algorithm-java/src/main/java/com/leetcode/MergeKLists.java
+++ algorithm-java/src/main/java/com/leetcode/MergeKLists.java
@@ -2,41 +2,24 @@ package com.leetcode;
 
 import com.leetcode.common.ListNode;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.PriorityQueue;
 
 /**
- * merge k lists leetcode's 23
+ * leetcode 23. merge k lists
+ * <p>
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ * <p>
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
  *
  * @author clx 4/22/2019
  */
 public class MergeKLists {
 
 	/**
-	 * merge brute force
-	 *
-	 * @param lists
-	 * @return
-	 */
-	public static List<Integer> mergeBruteForce(List<List<Integer>> lists) {
-		if (lists == null || lists.isEmpty()) {
-			return Collections.emptyList();
-		}
-		List<Integer> retList = new ArrayList<>();
-		for (List<Integer> val : lists) {
-			retList.addAll(val);
-		}
-		Collections.sort(retList);
-		return retList;
-	}
-
-	/**
 	 * merge k lists by priority queue
 	 *
-	 * @param listNodes
-	 * @return
+	 * @param listNodes listNode array
+	 * @return sorted listNode
 	 */
 	public static ListNode mergePriorityQueue(ListNode[] listNodes) {
 		if (listNodes == null || listNodes.length == 0) {
--- algorithm-java/src/test/java/com/leetcode/TestMergeKLists.java
+++ algorithm-java/src/test/java/com/leetcode/TestMergeKLists.java
@@ -4,30 +4,12 @@ import com.leetcode.common.ListNode;
 import com.leetcode.util.Stopwatch;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 /**
  * @author clx 4/22/2019
  */
 public class TestMergeKLists {
-
-	@Test
-	public void testMergeBruteForce() {
-		List<List<Integer>> lists = new ArrayList<>();
-		for (int idx = 0; idx < 10; idx++) {
-			List<Integer> list = new ArrayList<>(10);
-			for (int jdx = 0; jdx < 10; jdx++) {
-				list.add(new Random().nextInt(100));
-			}
-			lists.add(list);
-		}
-		Stopwatch stopwatch = new Stopwatch();
-		List<Integer> retList = MergeKLists.mergeBruteForce(lists);
-		System.out.println(stopwatch.elapsedNanoTime());
-		System.out.println(retList);
-	}
 
 	@Test
 	public void testMerge() {
@@ -40,33 +22,6 @@ public class TestMergeKLists {
 		Stopwatch stopwatch = new Stopwatch();
 		ListNode result = MergeKLists.mergePriorityQueue(listNodes);
 		System.out.println(stopwatch.elapsedNanoTime());
-		System.out.println(result);
-	}
-
-	@Test
-	public void testMergeCompare() {
-		List<List<Integer>> lists = new ArrayList<>();
-		for (int idx = 0; idx < 1000; idx++) {
-			List<Integer> list = new ArrayList<>(1000);
-			for (int jdx = 0; jdx < 1000; jdx++) {
-				list.add(new Random().nextInt(1000));
-			}
-			lists.add(list);
-		}
-		Stopwatch stopwatch = new Stopwatch();
-		List<Integer> retList = MergeKLists.mergeBruteForce(lists);
-		System.out.println(stopwatch.elapsedNanoTime());
-		System.out.println(retList);
-
-		ListNode[] listNodes = new ListNode[1000];
-		for (int idx = 0; idx < 1000; idx++) {
-			ListNode list = new ListNode(new Random().nextInt(1000));
-			list.setNext(new Random().nextInt(1000)).setNext(new Random().nextInt(1000));
-			listNodes[idx] = list;
-		}
-		Stopwatch sw = new Stopwatch();
-		ListNode result = MergeKLists.mergePriorityQueue(listNodes);
-		System.out.println(sw.elapsedNanoTime());
 		System.out.println(result);
 	}
 }
