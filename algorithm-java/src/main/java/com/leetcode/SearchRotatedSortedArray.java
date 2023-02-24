29963a755cf32036800b099659684ad29d5c006f
clx <chenlixin@stars-lighting.com>
2023-02-24 14:32:08 +0800

    feat:add search rotated sorted array

--- algorithm-java/src/main/java/com/leetcode/SearchRotatedSortedArray.java
+++ algorithm-java/src/main/java/com/leetcode/SearchRotatedSortedArray.java
@@ -47,4 +47,40 @@ public class SearchRotatedSortedArray {
 		}
 		return -1;
 	}
+
+	/**
+	 * search target
+	 *
+	 * @param nums   rotated array
+	 * @param target rotated num
+	 * @return rotated num idx
+	 */
+	public static int binarySearch(int[] nums, int target) {
+		if (ArrayUtils.checkArrayValid(nums)) {
+			return -1;
+		}
+		int lo = 0;
+		int hi = nums.length - 1;
+		int mid = 0;
+		while (lo <= hi) {
+			mid = lo + (hi - lo) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			}
+			if (nums[mid] >= nums[lo]) {
+				if (target < nums[mid] && target >= nums[lo]) {
+					hi = mid - 1;
+				} else {
+					lo = mid + 1;
+				}
+			} else {
+				if (target > nums[mid] && target <= nums[hi]) {
+					lo = mid + 1;
+				} else {
+					hi = mid - 1;
+				}
+			}
+		}
+		return -1;
+	}
 }
--- algorithm-java/src/test/java/com/leetcode/TestSearchRotatedSortedArray.java
+++ algorithm-java/src/test/java/com/leetcode/TestSearchRotatedSortedArray.java
@@ -19,4 +19,17 @@ public class TestSearchRotatedSortedArray {
 		int[] caseThree = {1};
 		System.out.println(SearchRotatedSortedArray.search(caseThree, 0));
 	}
+
+	@Test
+	void testBinarySearchArray() {
+		// case 1
+		int[] caseOne = {4, 5, 6, 7, 0, 1, 2};
+		System.out.println(SearchRotatedSortedArray.binarySearch(caseOne, 0));
+		// case 2
+		int[] caseTwo = {4, 5, 6, 7, 0, 1, 2};
+		System.out.println(SearchRotatedSortedArray.binarySearch(caseTwo, 3));
+		// case 3
+		int[] caseThree = {1};
+		System.out.println(SearchRotatedSortedArray.binarySearch(caseThree, 0));
+	}
 }
