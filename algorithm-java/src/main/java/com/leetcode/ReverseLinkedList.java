414e775677f32f7d9d500560559b89e2870cb34e
clx <chenlixin@stars-lighting.com>
2023-06-14 12:57:31 +0800

    feat:add reverse list node

---
+++ algorithm-java/src/main/java/com/leetcode/ReverseLinkedList.java
@@ -0,0 +1,50 @@
+package com.leetcode;
+
+import com.leetcode.common.ListNode;
+
+/**
+ * leetcode.206
+ * 给你单链表的头节点 head,请你反转链表，并返回反转后的链表。
+ *
+ * @author clx
+ */
+public class ReverseLinkedList {
+
+	/**
+	 * reverse linkedList with recursive
+	 *
+	 * @param head {@link ListNode}
+	 * @return {@link ListNode}
+	 */
+	public static ListNode reverseWithRecursive(ListNode head) {
+		if (head == null || head.getNext() == null) {
+			return head;
+		}
+		ListNode reverseHead = reverseWithRecursive(head.getNext());
+		head.getNext().setNext(head);
+		head.setNext(null);
+		return reverseHead;
+	}
+
+	/**
+	 * reverse linkedList with loop
+	 *
+	 * @param head {@link ListNode}
+	 * @return {@link ListNode}
+	 */
+	public static ListNode reverseWithLoop(ListNode head) {
+		if (head == null || head.getNext() == null) {
+			return head;
+		}
+		ListNode preNode = null;
+		ListNode currNode = head;
+		ListNode nextNode;
+		while (currNode != null) {
+			nextNode = currNode.getNext();
+			currNode.setNext(preNode);
+			preNode = currNode;
+			currNode = nextNode;
+		}
+		return preNode;
+	}
+}
---
+++ algorithm-java/src/test/java/com/leetcode/TestReverseLinkedList.java
@@ -0,0 +1,50 @@
+package com.leetcode;
+
+import com.leetcode.common.ListNode;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author clx
+ */
+public class TestReverseLinkedList {
+
+	@Test
+	public void testReverseWithRecursive() {
+		// case 1
+		ListNode caseOne = new ListNode(1);
+		caseOne.setNext(2).setNext(3).setNext(4).setNext(5);
+		System.out.println(caseOne);
+		System.out.println(ReverseLinkedList.reverseWithRecursive(caseOne));
+
+		// case 2
+		ListNode caseTwo = new ListNode(1);
+		caseTwo.setNext(2);
+		System.out.println(caseTwo);
+		System.out.println(ReverseLinkedList.reverseWithRecursive(caseTwo));
+
+		// case 3
+		ListNode caseThree = new ListNode();
+		System.out.println(caseThree);
+		System.out.println(ReverseLinkedList.reverseWithRecursive(caseThree));
+	}
+
+	@Test
+	public void testReverseWithLoop() {
+		// case 1
+		ListNode caseOne = new ListNode(1);
+		caseOne.setNext(2).setNext(3).setNext(4).setNext(5);
+		System.out.println(caseOne);
+		System.out.println(ReverseLinkedList.reverseWithLoop(caseOne));
+
+		// case 2
+		ListNode caseTwo = new ListNode(1);
+		caseTwo.setNext(2);
+		System.out.println(caseTwo);
+		System.out.println(ReverseLinkedList.reverseWithLoop(caseTwo));
+
+		// case 3
+		ListNode caseThree = new ListNode();
+		System.out.println(caseThree);
+		System.out.println(ReverseLinkedList.reverseWithLoop(caseThree));
+	}
+}
