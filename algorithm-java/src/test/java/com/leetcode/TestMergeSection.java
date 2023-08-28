2eaaefdbef3f84de02253345e884970059363ef0
clx <brianchen.clx@gmail.com>
2023-08-28 15:12:41 +0800

    fix:mod test case

--- algorithm-java/src/test/java/com/leetcode/TestMergeSection.java
+++ algorithm-java/src/test/java/com/leetcode/TestMergeSection.java
@@ -13,10 +13,12 @@ public class TestMergeSection {
 	public void testMergeSection() {
 		// case 1
 		int[][] caseOne = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
-		System.out.println(Arrays.deepToString(MergeSection.mergeSection(caseOne)));
+		System.out.println("before:" + Arrays.deepToString(caseOne));
+		System.out.println("after:" + Arrays.deepToString(MergeSection.mergeSection(caseOne)));
 		// case 2
 		int[][] caseTwo = {{1, 4}, {4, 5}};
-		System.out.println(Arrays.deepToString(MergeSection.mergeSection(caseTwo)));
+		System.out.println("before:" + Arrays.deepToString(caseTwo));
+		System.out.println("after:" + Arrays.deepToString(MergeSection.mergeSection(caseTwo)));
 	}
 
 	@Test
