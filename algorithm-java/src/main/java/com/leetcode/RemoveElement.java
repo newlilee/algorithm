8c08b5efea819bac25d622ddfe95dc065bbb70bc
clx <brianchen.clx@gmail.com>
2023-10-11 21:03:50 +0800

    feat:add 27

--- algorithm-java/src/main/java/com/leetcode/RemoveElement.java
+++ algorithm-java/src/main/java/com/leetcode/RemoveElement.java
@@ -1,24 +1,31 @@
 package com.leetcode;
 
-import com.leetcode.util.ArrayUtils;
-
 /**
- * 27. Remove Element
+ * leetcode.27 Remove Element
  *
  * @author clx
  */
 public class RemoveElement {
 
-	public static int removeElement(int[] array, int val) {
-		if (ArrayUtils.checkArrayValid(array)) {
+	/**
+	 * remove element brute force
+	 *
+	 * @param nums array
+	 * @param val  target val
+	 * @return new array len
+	 */
+	public static int removeElementBruteForce(int[] nums, int val) {
+		if (nums == null || nums.length == 0) {
 			return -1;
 		}
 		int count = 0;
-		for (int idx = 1; idx < array.length; idx++) {
-			if (val != array[idx]) {
+		for (int num : nums) {
+			if (val != num) {
 				count++;
 			}
 		}
-		return count + 1;
+		return count;
 	}
+
+
 }
--- algorithm-java/src/test/java/com/leetcode/TestRemoveElement.java
+++ algorithm-java/src/test/java/com/leetcode/TestRemoveElement.java
@@ -1,5 +1,6 @@
 package com.leetcode;
 
+import com.leetcode.util.ArrayUtils;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -8,9 +9,26 @@ import org.junit.jupiter.api.Test;
 public class TestRemoveElement {
 
 	@Test
+	public void testRemoveBruteForce() {
+		// case 1
+		int[] caseOne = {3, 2, 2, 3};
+		System.out.println(RemoveElement.removeElementBruteForce(caseOne, 3));
+		ArrayUtils.printArray(caseOne);
+		// case 2
+		int[] caseTwo = {0, 1, 2, 2, 3, 0, 4, 2};
+		System.out.println(RemoveElement.removeElementBruteForce(caseTwo, 2));
+		ArrayUtils.printArray(caseTwo);
+	}
+
+	@Test
 	public void testRemove() {
-		int[] array = new int[]{0, 1, 2, 2, 3, 0, 4, 2};
-		int result = RemoveElement.removeElement(array, 2);
-		System.out.println("result:" + result);
+		// case 1
+		int[] caseOne = {3, 2, 2, 3};
+		System.out.println(RemoveElement.removeElement(caseOne, 3));
+		ArrayUtils.printArray(caseOne);
+		// case 2
+		int[] caseTwo = {0, 1, 2, 2, 3, 0, 4, 2};
+		System.out.println(RemoveElement.removeElement(caseTwo, 2));
+		ArrayUtils.printArray(caseTwo);
 	}
 }
